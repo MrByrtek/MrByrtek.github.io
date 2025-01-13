0f81bbce686ca0e3b9733efb40d659fa205d8a0e
MrByrtek <tlomiej2137@gmail.com>
2025-01-13 09:26:41 +0100

    Update ClickerScript.js

--- ClickerScript.js
+++ ClickerScript.js
@@ -1,17 +1,64 @@
 let button = document.querySelector("#click")
 let pointDisplay = document.querySelector("#points")
 let passiveDisplay = document.querySelector("#passive")
+let pointsAll = document.querySelector("#pointsall")
+let openshop = document.querySelector("#openShop")
 
 let points = 0;
 let passive = 0;
+
+let upgrades = [];
+let upgradesCost = [];
+upgrades[0] = document.querySelector("#upgrade1")
+upgradesCost[0] = 5
+
 pointDisplay.innerHTML = points;
 passiveDisplay.innerHTML = passive;
 
-button.addEventListener("click", function(){
+function show() {
+    pointDisplay.innerHTML = points.toFixed(1);
+    passiveDisplay.innerHTML = passive.toFixed(1);
+}
+
+button.addEventListener("click", function () {
     points++;
-    pointDisplay.innerHTML = points;
+    pointsAll.classList.add("animation");
+    setTimeout(function () {
+        pointsAll.classList.remove("animation")
+    }, 150)
+    show();
 })
 
-setInterval(function(){
-    
+let increment = [];
+increment[0] = 1;
+
+upgrades[0].addEventListener("click", function () {
+    if (points >= upgradesCost[0]) {
+        points -= upgradesCost[0];
+        passive += 0.1;
+        upgradesCost[0] += increment[0]
+        increment[0]*2;
+    }
+    show();
+})
+
+setInterval(function () {
+    points += passive / 100;
+    show();
+}, 10)
+let openClosed = true
+openshop.addEventListener("click", function () {
+    let shopDisplay = document.querySelector("#sklep");
+    if (openClosed) {
+        openshop.innerHTML = "Zamknij sklep"
+        shopDisplay.style.setProperty("height", "100%");
+        shopDisplay.style.setProperty("display", "block");
+        openClosed = false;
+    }
+    else{
+        openshop.innerHTML = "Otworz sklep"
+        shopDisplay.style.setProperty("height", "0px");
+        shopDisplay.style.setProperty("display", "none")
+        openClosed = true;
+    }
 })
