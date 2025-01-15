263c36ec37c7d717ca63d98ea9c4db1c30324227
MrByrtek <tlomiej2137@gmail.com>
2025-01-15 19:39:45 +0100

    Update ClickerScript.js

--- ClickerScript.js
+++ ClickerScript.js
@@ -1,16 +1,18 @@
 let button = document.querySelector("#click")
 let pointDisplay = document.querySelector("#points")
 let passiveDisplay = document.querySelector("#passive")
+let passiveGainDisplay = document.querySelector("#passiveGain")
 let pointsAll = document.querySelector("#pointsall")
 let openshop = document.querySelector("#openShop")
 
 let points = 0;
 let passive = 0;
+let clickstrength = 1;
+let passiveGain = 0.1;
 
 let upgrades = [];
 let upgradesCost = [];
-upgrades[0] = document.querySelector("#upgrade1")
-upgradesCost[0] = 5
+let increment = [];
 
 pointDisplay.innerHTML = points;
 passiveDisplay.innerHTML = passive;
@@ -21,24 +23,11 @@ function show() {
 }
 
 button.addEventListener("click", function () {
-    points++;
+    points+=clickstrength;
     pointsAll.classList.add("animation");
     setTimeout(function () {
         pointsAll.classList.remove("animation")
-    }, 150)
-    show();
-})
-
-let increment = [];
-increment[0] = 1;
-
-upgrades[0].addEventListener("click", function () {
-    if (points >= upgradesCost[0]) {
-        points -= upgradesCost[0];
-        passive += 0.1;
-        upgradesCost[0] += increment[0]
-        increment[0]*2;
-    }
+    }, 50)
     show();
 })
 
@@ -46,19 +35,88 @@ setInterval(function () {
     points += passive / 100;
     show();
 }, 10)
+
+//Shop
 let openClosed = true
 openshop.addEventListener("click", function () {
     let shopDisplay = document.querySelector("#sklep");
     if (openClosed) {
         openshop.innerHTML = "Zamknij sklep"
-        shopDisplay.style.setProperty("height", "100%");
-        shopDisplay.style.setProperty("display", "block");
+        shopDisplay.style.setProperty("opacity", "100%");
         openClosed = false;
     }
     else{
         openshop.innerHTML = "Otworz sklep"
-        shopDisplay.style.setProperty("height", "0px");
-        shopDisplay.style.setProperty("display", "none")
+        shopDisplay.style.setProperty("opacity", "0%")
         openClosed = true;
     }
+})
+
+// First upgrade
+upgrades[0] = document.querySelector("#upgrade0")
+upgradesCost[0] = 5
+increment[0] = 1;
+let upgrade0cost = document.querySelector("#upgrade0cost");
+
+upgrades[0].addEventListener("click", function () {
+    if (points >= upgradesCost[0]) {
+        points -= upgradesCost[0];
+        passive += passiveGain;
+        upgradesCost[0] += increment[0]
+        upgrade0cost.innerHTML = upgradesCost[0];
+        increment[0]+=0.5;
+    }
+})
+
+// Second upgrade
+upgrades[1] = document.querySelector("#upgrade1")
+upgradesCost[1] = 50
+increment[1] = 50;
+let upgrade1cost = document.querySelector("#upgrade1cost");
+
+upgrades[1].addEventListener("click", function () {
+    if (points >= upgradesCost[1]) {
+        points -= upgradesCost[1];
+        clickstrength += 1;
+        upgradesCost[1] += increment[1]
+        upgrade1cost.innerHTML = upgradesCost[1];
+        increment[1]*=2;
+    }
+})
+
+// Third upgrade
+upgrades[2] = document.querySelector("#upgrade2")
+upgradesCost[2] = 1000;
+increment[2] = 1000;
+let upgrade2cost = document.querySelector("#upgrade2cost");
+
+upgrades[2].addEventListener("click", function () {
+    if (points >= upgradesCost[2]) {
+        points -= upgradesCost[2];
+        passiveGain*=2;
+        passive*=2;
+        upgradesCost[2] += increment[2]
+        passiveGainDisplay.innerHTML = passiveGain;
+        upgrade2cost.innerHTML = upgradesCost[2];
+        increment[2]*=2;
+    }
+})
+
+upgrade0cost.innerHTML = upgradesCost[0];
+upgrade1cost.innerHTML = upgradesCost[1];
+upgrade2cost.innerHTML = upgradesCost[2];
+
+let pattern = "";
+document.addEventListener("keydown", function(e){
+    if(e.key == "Enter"){
+        if(pattern == "ArrowUpArrowUpArrowDownArrowDownArrowLeftArrowRightArrowLeftArrowRightab"){
+            points+=100000;
+            pattern = "";
+        }
+        else
+            pattern = "";
+    }
+    else
+        pattern += e.key
+        
 })
